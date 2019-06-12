ac902b5212e9542fb20ac807295db3dcf978109c
Stepan Shabalin <stomperhomp@gmail.com>
2019-06-12 20:42:39 +0700

    added surface lists

--- lib/surface.go
+++ lib/surface.go
@@ -1,6 +1,26 @@
 package lib
 
+import "math"
+
 // Surface is an interface for any hittable surface
 type Surface interface {
-	hit(r Ray) Hit
+	Hit(r Ray) Hit
+}
+
+// SurfaceList is a list of surfaces
+type SurfaceList []Surface
+
+// Hit is an implementation of the hit function for surface lists
+func (l SurfaceList) Hit(r Ray) Hit {
+	minDistanceHit := Hit{}
+	minDistance := math.Inf(1)
+	for _, surf := range l {
+		hit := surf.Hit(r)
+		distance := hit.position.sub(r.position).mag()
+		if(distance < minDistance) {
+			minDistance = distance
+			minDistanceHit = hit
+		}
+	}
+	return minDistanceHit
 }
