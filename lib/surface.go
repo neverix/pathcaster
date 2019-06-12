de88400252f0038a4893da814b06e2ffffff544b
Stepan Shabalin <stomperhomp@gmail.com>
2019-06-12 21:04:18 +0700

    changed surface hit signature

--- lib/surface.go
+++ lib/surface.go
@@ -4,22 +4,24 @@ import "math"
 
 // Surface is an interface for any hittable surface
 type Surface interface {
-	Hit(r Ray) Hit
+	Hit(r *Ray) *Hit
 }
 
 // SurfaceList is a list of surfaces
 type SurfaceList []Surface
 
 // Hit is an implementation of the hit function for surface lists
-func (l SurfaceList) Hit(r Ray) Hit {
-	minDistanceHit := Hit{}
+func (l SurfaceList) Hit(r *Ray) *Hit {
+	minDistanceHit := new(Hit)
 	minDistance := math.Inf(1)
 	for _, surf := range l {
 		hit := surf.Hit(r)
-		distance := hit.position.Sub(r.Position).Mag()
-		if(distance < minDistance) {
-			minDistance = distance
-			minDistanceHit = hit
+		if hit != nil {
+			distance := hit.position.Sub(r.Position).Mag()
+			if(distance < minDistance) {
+				minDistance = distance
+				minDistanceHit = hit
+			}
 		}
 	}
 	return minDistanceHit
