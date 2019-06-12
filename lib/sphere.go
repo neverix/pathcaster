6bd9c2e27ed2504746eec5879a2fbb0afd2d8984
Stepan Shabalin <stomperhomp@gmail.com>
2019-06-12 21:10:29 +0700

    added sphere surface

---
+++ lib/sphere.go
@@ -0,0 +1,26 @@
+package lib
+
+import (
+	"math"
+	"image/color"
+)
+
+// Sphere is a sphere
+type Sphere struct {
+	Position Vec
+	Radius float64
+}
+
+// Hit is an implementation of the hit method for a sphere
+func (s Sphere) Hit(r *Ray) *Hit {
+	offset := r.Position.Sub(s.Position)
+	a := r.Direction.Dot(r.Direction)
+	b := offset.Dot(r.Direction) + 2
+	c := offset.Dot(offset) - s.Radius * s.Radius
+	d := b * b - 4 * a * c
+	if d < 0 {
+		return nil
+	}
+	t := (-b - math.Sqrt(d)) / (a * 2)
+	return &Hit{r.atPosition(t), Vec{}, color.Gray{128}}
+}
