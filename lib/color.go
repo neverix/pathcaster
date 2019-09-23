af63bd6892f4fd953769c9da8bd3c6c4e8d2e1c0
Stepan Shabalin <stomperhomp@gmail.com>
2019-09-23 08:32:27 +0300

    added HDR

--- lib/color.go
+++ lib/color.go
@@ -1,14 +1,18 @@
 package lib
 
 // Color is an HDR replacement to Go's image/color.Color
-type Color Vec
+type Color struct {
+	R float64
+	G float64
+	B float64
+}
 
 // Unwrap turns a color into a multiple-return
 func (c Color) Unwrap() (float64, float64, float64) {
-	return c.X, c.Y, c.Z
+	return c.R, c.G, c.B
 }
 
 // Multiply multiplies colors
 func (c Color) Multiply(o Color) Color {
-	return Color{c.X * o.X, c.Y * o.Y, c.Z * o.Z}
+	return Color{c.R * o.R, c.G * o.G, c.B * o.B}
 }
--- main.go
+++ main.go
@@ -6,36 +6,44 @@ import (
 	"image/color"
 	"image/png"
 	"log"
+	"math"
 	"os"
+
 	"github.com/neverix/pathcaster/lib"
 )
 
 const (
-	width = 100
-	height = 50
-	samples = 100
+	width    = 100
+	height   = 50
+	samples  = 100
 	maxDepth = 50
 )
 
 func main() {
 	var surfaces lib.SurfaceList = []lib.Surface{
 		&lib.Background{Color: lib.Color{
-			X: 1, Y: 1, Z: 1}},
+			R: 0, G: 0, B: 0}},
+		&lib.Sphere{
+			Position: lib.Vec{
+				X: 0, Y: -9, Z: 15},
+			Radius: 4,
+			Shader: &lib.EmissiveShader{Color: lib.Color{
+				R: 0, G: 0, B: 100}}},
 		&lib.Sphere{
 			Position: lib.Vec{
 				X: 0, Y: 94, Z: 15},
 			Radius: 90,
 			Shader: &lib.DiffuseShader{Color: lib.Color{
-				X: 0, Y: 1, Z: 0}}},
+				R: 0, G: 1, B: 0}}},
 		&lib.Sphere{
 			Position: lib.Vec{
 				X: 0, Y: 0, Z: 15},
 			Radius: 4,
 			Shader: &lib.DiffuseShader{Color: lib.Color{
-				X: 1, Y: 0, Z: 0}}}}
+				R: 1, G: 0, B: 0}}}}
 	camera := lib.Camera{
-		Position: lib.Vec{X: 0, Y: 0, Z: 0.3},
-		ScreenWidth: width,
+		Position:     lib.Vec{X: 0, Y: 0, Z: 0.3},
+		ScreenWidth:  width,
 		ScreenHeight: height}
 
 	canvas := image.NewRGBA(image.Rect(0, 0, width, height))
@@ -48,12 +56,11 @@ func main() {
 				rTotal += r
 				gTotal += g
 				bTotal += b
-
 			}
 			color := color.RGBA{
-				uint8(rTotal / samples * 255),
-				uint8(gTotal / samples * 255),
-				uint8(bTotal / samples * 255),
+				uint8(math.Min(rTotal/samples*255, 255)),
+				uint8(math.Min(gTotal/samples*255, 255)),
+				uint8(math.Min(bTotal/samples*255, 255)),
 				255}
 			canvas.Set(x, y, color)
 		}
