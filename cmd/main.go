331af640b9e2bdccf32fa8dbb33315363bfd0a51
Stepan Shabalin <stomperhomp@gmail.com>
2019-09-26 09:01:37 +0300

    added teapot test

--- cmd/main.go
+++ cmd/main.go
@@ -14,18 +14,19 @@ import (
 )
 
 const (
-	width    = 200
-	height   = 100
-	samples  = 64
+	width    = 100
+	height   = 50
+	samples  = 3
 	maxDepth = 3
 )
 
 func main() {
-	pyramid, err := surfaces.ParseOBJFile("data/pyramid.obj")
+	model, err := surfaces.ParseOBJFile("data/big/teapot.obj")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	fmt.Println("Parsed!")
 	var world surfaces.SurfaceList = []pathcaster.Surface{
 		&surfaces.Background{Color: util.Color{
 			R: 0.5, G: 0.7, B: 1}},
@@ -47,15 +48,16 @@ func main() {
 			Radius: 4,
 			Shader: &shaders.ReflectiveShader{Color: util.Color{
 				R: 0.8, G: 0.6, B: 0.2}}},
-		pyramid.ToSurface()}
+		model.ToSurface()}
 
 	camera := pathcaster.Camera{
-		Position:     util.Vec{X: 0, Y: 0, Z: -6},
+		Position:     util.Vec{X: 0, Y: 1, Z: -6},
 		ScreenWidth:  width,
 		ScreenHeight: height}
 	renderConfig := pathcaster.RenderConfig{
 		MaxDepth: maxDepth,
 		Samples:  samples}
+	fmt.Println("Rendering...")
 	canvas := camera.RenderSurface(world, renderConfig)
 
 	outputFile, err := os.Create("render.png")
